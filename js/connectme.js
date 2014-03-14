115158fac9f656c8362f914fd95e70b342d1b3ab
Duncan Watson <duncan.watson@technophobia.com>
2014-03-14 15:43:26 +0000

    Added stuff

--- js/connectme.js
+++ js/connectme.js
@@ -13,7 +13,7 @@ jQuery(document).ready(function() {
 	});		
 	  
 
-	d3.json("http://172.30.128.106:8080/node/11_1422/2",
+	d3.json("http://172.30.128.106:8080/node/11_5/2",
 		function(error, graph) {
 		// d3.json("/test2.json", function(error, graph) {
 		  force.addNodes(graph.nodes, graph.links);
@@ -22,4 +22,19 @@ jQuery(document).ready(function() {
 
 	// d3.json("http://172.30.128.106:8080/node/rid/depth")
 	
+
+	jQuery('#people-toggle').click(function() {
+		var button = jQuery(this);
+		if (button.hasClass('btn-success')) {
+			button.removeClass('btn-success');
+			button.addClass('btn-default');
+			force.removePeopleFilter();
+		} else {
+			button.addClass('btn-success');
+			button.removeClass('btn-default');
+			force.addPeopleFilter();
+		}
+	});
+
+
 });
--- js/force-directed-graph.js
+++ js/force-directed-graph.js
@@ -9,8 +9,8 @@ ForceDirectedGraph = function(config) {
 	this.textGroup = this.svg.append('g');
 
 	this.force = d3.layout.force()
-	    .charge(-30)
-	    .linkDistance(30)
+	    .charge(-100)
+	    .linkDistance(100)
 	    .size([this.pane.width, this.pane.height]);
 
   this.force
@@ -21,10 +21,6 @@ ForceDirectedGraph = function(config) {
 ForceDirectedGraph.prototype.addNodes = function(nodes, links) {
 
 	var _this = this;
-	var nodeGroup = this.nodeGroup;
-	var textGroup = this.textGroup;
-
-	var color = d3.scale.category20();
 	
 	this.nodes = this.nodes.concat(nodes);
 	    
@@ -40,6 +36,16 @@ ForceDirectedGraph.prototype.addNodes = function(nodes, links) {
 	});
 
 	this.links = this.links.concat(edges);
+
+	this.refresh();
+};
+
+ForceDirectedGraph.prototype.refresh = function() {
+
+	var nodeGroup = this.nodeGroup;
+	var textGroup = this.textGroup;
+
+	var color = d3.scale.category20();
 
 	this.force
     .nodes(this.nodes)
@@ -63,22 +69,45 @@ ForceDirectedGraph.prototype.addNodes = function(nodes, links) {
     // .append("circle")
     .append("svg:image")
       .attr("xlink:href", function(d) {
-      	if (d.class == "User") {
-      		return "/images/1394715037_location-24.svg";
+      	if (d.classname == "User") {
+      		return "/images/user.svg";
+      	} else if (d.classname == "Group") {
+					return "/images/network.svg";
+      	} else if (d.classname == "UserGroup") {
+					return "/images/usergroup.svg";
       	} else {
-					return "/images/1394715037_location-24.svg";
+					return "/images/org.svg";
       	}
       })
       .attr("id", function(d) { return d.rid; })        
       .attr("class", function(d) { return "node node_" + d.rid; })
-      .attr("r", function(d) { 
-        return d.class == 'User' ? 5 : 10;
+      .attr("r", function(d) {
+      	if (d.classname == "User") {
+      		return 15 + (Math.sqrt(Math.sqrt(d.edgesout)) * 2);
+      	} else {
+      		return 15 + (Math.sqrt(Math.sqrt(d.edgesin)) * 2);
+      	}
       })
-      .attr("width", function(d) { 
-        return d.class == 'User' ? 5 : 10;
+      .attr("width", function(d) {
+      	if (d.classname == "User") {
+      		return 15 + (Math.sqrt(Math.sqrt(d.edgesout)) * 2);
+      	} else {
+      		return 15 + (Math.sqrt(Math.sqrt(d.edgesin)) * 2);
+      	}
       })
-      .attr("height", function(d) { 
-        return d.class == 'User' ? 5 : 10;
+      .attr("height", function(d) {
+      	if (d.classname == "User") {
+      		return 15 + (Math.sqrt(Math.sqrt(d.edgesout)) * 2);
+      	} else {
+      		return 15 + (Math.sqrt(Math.sqrt(d.edgesin)) * 2);
+      	}
+      })
+      .attr("charge", function(d) {
+      	if (d.classname == "User") {
+      		return -(Math.sqrt(d.edgesout) * 2);
+      	} else {
+      		return -(Math.sqrt(d.edgesin) * 2);
+      	}
       })
       .style("fill", function(d) { return color(d.group); })
       .call(this.force.drag);
@@ -90,6 +119,8 @@ ForceDirectedGraph.prototype.addNodes = function(nodes, links) {
   node.each(function(thisData) {
     var thisD3Node = nodeGroup.select(".node_" + thisData.rid);
     thisData.originalR = thisD3Node.attr("r");
+    thisData.originalWidth = thisD3Node.attr("width");
+    thisData.originalHeight = thisD3Node.attr("height");
   });
 
 
@@ -98,7 +129,7 @@ ForceDirectedGraph.prototype.addNodes = function(nodes, links) {
   // this.mouseupNode(node);
   this.mouseoverNode(node);
   this.mouseoutNode(node);
-  this.mouseoverLink(link);
+  // this.mouseoverLink(link);
 
   
 
@@ -110,17 +141,21 @@ ForceDirectedGraph.prototype.addNodes = function(nodes, links) {
 
     node.attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; })
-        .attr("x", function(d) { return d.x; })
-        .attr("y", function(d) { return d.y; });
+        .attr("x", function(d) { return (d.x - d.originalWidth / 2); })
+        .attr("y", function(d) { return (d.y - d.originalHeight / 2); });
   });
 
   this.force.start();
 };
 
+
 ForceDirectedGraph.prototype.clickNode = function(nodes) {
 	var _this = this;
 	nodes.on("click", function(d) {
-	  _this.selectedNode = this;
+
+	  // if (d.selected) {
+
+	  // } esl
 	  d3.json("http://172.30.128.106:8080/node/" + d.rid + "/2",
 			function(error, graph) {
 
@@ -183,7 +218,9 @@ ForceDirectedGraph.prototype.mouseoverNode = function(nodes) {
     
     thisD3Node
       .transition()
-      .attr("r", thisData.originalR * 2);
+      .attr("r", thisData.originalR * 2)
+      .attr("width", thisData.originalWidth * 2)
+      .attr("height", thisData.originalHeight * 2);
 
   });
 };
@@ -208,29 +245,71 @@ ForceDirectedGraph.prototype.mouseoutNode = function(nodes) {
 
     if (thisData.originalR != null) {
       thisD3Node.transition()
-        .attr("r", thisData.originalR);
+        .attr("r", thisData.originalR)
+        .attr("width", thisData.originalWidth)
+        .attr("height", thisData.originalHeight);
     }
   });
 };
 
 
-ForceDirectedGraph.prototype.mouseoverLink = function(links) {
-	var _this = this;
+// ForceDirectedGraph.prototype.mouseoverLink = function(links) {
+// 	var _this = this;
 
-	links.on("mouseover", function(d) {
-    _this.textGroup
-      .selectAll("text")
-      .remove();
+// 	links.on("mouseover", function(d) {
+//     _this.textGroup
+//       .selectAll("text")
+//       .remove();
 
-    var node1 = d.source;
-    var node2 = d.target;
-    var midX = (node1.x + node2.x) / 2;
-    var midY = (node1.y + node2.y) / 2;
+//     var node1 = d.source;
+//     var node2 = d.target;
+//     var midX = (node1.x + node2.x) / 2;
+//     var midY = (node1.y + node2.y) / 2;
 
-    _this.textGroup
-      .append("text")
-        .text(d.source.name + " to " + d.target.name)
-          .attr("x", midX + 10)
-          .attr("y", midY - 10)
-	  });
+//     _this.textGroup
+//       .append("text")
+//         .text(d.source.name + " to " + d.target.name)
+//           .attr("x", midX + 10)
+//           .attr("y", midY - 10)
+// 	  });
+// };
+
+
+ForceDirectedGraph.prototype.addPeopleFilter = function() {
+	var peopleNodes = this.nodes.filter(function(node) {
+		return node.class == "User";
+	});
+
+	var peopleLinks = this.links.filter(function(link) {
+		return link.source.class == "User" || link.target.class == "User";
+	});
+
+	this.removeNodes(peopleNodes, peopleLinks);
+
+};
+
+ForceDirectedGraph.prototype.removeNodes = function(nodesToRemove, linksToRemove) {
+
+	this.nodes = this.nodes.filter(function(node) {
+		for (var i = 0; i < nodesToRemove.length; i++) {
+			if (node == nodesToRemove) {
+				return false;
+			}
+		}
+
+		return true;
+	});
+
+	this.nodes = this.links.filter(function(links) {
+		for (var i = 0; i < linksToRemove.length; i++) {
+			if (links == linksToRemove) {
+				return false;
+			}
+		}
+
+		return true;
+	});
+
+	this.refresh();
+
 };
